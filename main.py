8174854300e7ed6eb287379abeb09a145dcd7413
Ney Eduardo <286360110+neykblz-full@users.noreply.github.com>
2026-06-26 09:03:50 -0300

    Cria estrutura inicial do projeto

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.agendakblz.app import iniciar_aplicacao
+
+
+if __name__ == "__main__":
+    iniciar_aplicacao()
+
---
+++ src/agendakblz/__init__.py
@@ -0,0 +1,2 @@
+"""Pacote principal da Agenda de Consultas da Equipe Kblz."""
+
